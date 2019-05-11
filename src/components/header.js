25cdc84b1aaae534041e13e35b709df28cbed529
berkay surmeli <berkaysurmeli@hotmail.com>
2019-05-10 22:15:39 -0700

    some css adjustments. decisions decisions

--- src/components/header.js
+++ src/components/header.js
@@ -3,14 +3,16 @@ import React from 'react'
 class Header extends React.Component {
   render () {
     return (
-      <div className="header-container">
+      <div className="container-fluid">
         <div className="logo"></div>
-        <nav className="nav">
+        <div>
+          <nav className="nav">
             <a href="index.html">Home</a>
-            <a href="/index.html">Products</a>
             <a href="/index.html">About Us</a>
-            <a className="last" href="/index.html">Contact</a>
-        </nav>
+            <a href="/index.html">Products</a>
+            <a className="last" href="/index.html">Contact Us</a>
+          </nav>
+        </div>
       </div>
     );
   }
