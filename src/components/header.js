7aed42abe92409afcbd5c19e5904a9575001433e
berkay surmeli <berkaysurmeli@hotmail.com>
2019-05-23 23:19:11 -0700

    animation and carousel added

--- src/components/header.js
+++ src/components/header.js
@@ -3,30 +3,29 @@ import React from "react";
 class Header extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isToggleOn: false };
-
+    this.state = { isToggleOn: true };
+    //binding
     this.toggleMenu = this.toggleMenu.bind(this);
   }
-
+  //toggle menu state for smaller devices
   toggleMenu = () => {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
     }));
   };
   render() {
     return (
       <div className="header">
         <div className="logo" />
-        <div onClick={this.toggleMenu} className="menu-button" />
-        {this.state.isToggleOn &&
-          <nav className="nav">
-            <a href="/index.html">About Us</a>
-            <a href="/index.html">Products</a>
-            <a className="last" href="/index.html">
-              Contact Us
-            </a>
-          </nav>
-        }
+        <div
+          onClick={this.toggleMenu}
+          className="menu-button"
+        />
+        <nav className={this.state.isToggleOn ? "off" : "nav"}>
+          <a href="/index.html">About Us</a>
+          <a href="/index.html">Products</a>
+          <a className="last" href="/index.html">Contact Us</a>
+        </nav>
       </div>
     );
   }
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,10 +1,11 @@
 import React from "react";
+import CCarousel from "./carousel/ccarousel.js";
 
 class Homepage extends React.Component {
   render() {
     return (
       <div className="homepage">
-        <p>Content</p>
+        <CCarousel />
       </div>
     );
   }
